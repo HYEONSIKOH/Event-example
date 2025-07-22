8e19dd9ec69f3a6afdeaf4bfe7ea2a9adcdf7cd4
HYEONSIK <ks06891@naver.com>
2025-07-22 16:49:41 +0900

    init: git

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Default
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Spring Web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// Redis(Lettuce) - 스핀락을 이용한 분산락
+	// implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// Redisson - Redis 클라이언트 라이브러리로, 분산락 기능을 제공
+	// implementation("org.redisson:redisson-spring-boot-starter:3.50.0")
+
+	// JPA & MariaDB
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+	// ARM 전용 (M Chip)
+	implementation("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/example/EventDomain/EventDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventDomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
